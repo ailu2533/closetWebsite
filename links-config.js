2aee782248a379d6b2e80679e7b7ede4f9323f1e
ailu <im.ailu@outlook.com>
2025-08-28 17:44:44 +0800

    更新隐私协议

--- links-config.js
+++ links-config.js
@@ -5,7 +5,7 @@ const DOWNLOAD_LINKS = {
     IOS_APP_STORE: "https://apps.apple.com/cn/app/%E6%9F%9A%E5%AD%90%E8%A1%A3%E6%A9%B1-%E6%99%BA%E8%83%BD%E7%A7%81%E4%BA%BA%E8%A1%A3%E6%A9%B1%E7%AE%A1%E5%AE%B6/id6504781666",
     
     // 安卓版下载链接
-    ANDROID_LEMON_WARDROBE: "https://wwcu.lanzoub.com/iQ6Fi349f3qb",
+    ANDROID_LEMON_WARDROBE: "https://wwcu.lanzoub.com/iJ3NF34rpz7c",
     
     // 数据修复版下载链接
     ANDROID_DATA_REPAIR: "https://wwcu.lanzoub.com/i5ihF349dlzg"
